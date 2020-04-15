b2c031f5beb9be9fff99c02c777a45cfc030bf6d
natlibesa <esa.kaalikoski@helsinki.fi>
2020-04-15 10:14:01 +0300

    Modified

--- src/filter.js
+++ src/filter.js
@@ -1,3 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = _default;
+
+var _moment = _interopRequireDefault(require("moment"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
 *
 * @licstart  The following is the entire license notice for the JavaScript code in this file.
@@ -25,104 +36,111 @@
 * for the JavaScript code in this file.
 *
 */
+const EXCLUDED_MATERIAL_TYPES = ['d', 'e', 'q', '4', '6', 'j', '7', 'n', 'f'];
 
-import moment from 'moment';
 
-const EXCLUDED_MATERIAL_TYPES = [
-	'd',
-	'e',
-	'q',
-	'4',
-	'6',
-	'j',
-	'7',
-	'n',
-	'f'
-];
+function _default(record, earliestCatalogTime) {
 
-export default function (record, earliestCatalogTime) {
-	const leader = record.varFields.find(f => f.fieldTag === '_');
-	const materialType = record.materialType.code.trim();
+    let materialType = "";
 
-	if (!checkLeader()) {
-		return false;
-	}
+if (record.materialType === undefined) {
+  materialType = "temporary";  
+  return false;
 
-	if (!record.varFields.find(f => f.marcTag === '008')) {
-		return false;
-	}
+}  else {
 
-	// Uncomment this to filter out records with 007 (For testing)
-	/* if (record.varFields.find(f => f.marcTag === '007')) {
-		return false;
-	} */
+      if (record.materialType && record.materialType.code) {
+        materialType = record.materialType.code.trim();    
+      } else {
+        return false;
+      }
 
-	if (EXCLUDED_MATERIAL_TYPES.includes(materialType)) {
-		return false;
-	}
-
-	if (isFromOverDrive()) {
-		return false;
-	}
-
-	if (!record.catalogDate || !moment(record.catalogDate).isValid()) {
-		return false;
-	}
-
-	if (earliestCatalogTime && moment(record.catalogDate).isBefore(earliestCatalogTime)) {
-		return false;
-	}
-
-	return true;
-
-	function checkLeader() {
-		if (!leader) {
-			return false;
-		}
-
-		if (leader.content[17] !== '4') {
-			return false;
-		}
-
-		if (['c', 'd', 'j'].includes(leader.content[6])) {
-			return false;
-		}
-
-		if (isMap()) {
-			return false;
-		}
-
-		return true;
-
-		function isMap() {
-			return leader.content[6] === 'a' && record.varFields.some(f => {
-				if (f.marcTag === '655') {
-					const a = f.subfields.find(sf => sf.tag === 'a');
-
-					if (a && a.content === 'kartastot') {
-						return true;
-					}
-				}
-
-				return false;
-			});
-		}
-	}
-
-	function isFromOverDrive() {
-		const f037 = record.varFields.filter(f => f.marcTag === '037');
-		const f710 = record.varFields.filter(f => f.marcTag === '710');
-
-		return f037.some(match037) || f710.some(match710);
-
-		function match037(f) {
-			const b = f.subfields.find(sf => sf.tag === 'b' && /^OverDrive/.test(sf.content));
-			const n = f.subfields.find(sf => sf.tag === 'n' && sf.content === 'http://www.overdrive.com');
-			return b && n;
-		}
-
-		function match710(f) {
-			return f.subfields.find(sf => sf.tag === 'a' && /^overdrive/i.test(sf.content));
-		}
-	}
 }
+
+
+
+  const leader = record.varFields.find(f => f.fieldTag === '_');        
+  
+
+
+  if (!checkLeader()) {   
+    return false;
+  }
+
+  if (!record.varFields.find(f => f.marcTag === '008')) {
+    return false;
+  } // Uncomment this to filter out records with 007 (For testing)
+  /* if (record.varFields.find(f => f.marcTag === '007')) {
+  	return false;
+  } */
+
+
+  if (EXCLUDED_MATERIAL_TYPES.includes(materialType)) {
+    return false;
+  }
+
+  if (isFromOverDrive()) {
+    return false;
+  }
+
+  if (!record.catalogDate || !(0, _moment.default)(record.catalogDate).isValid()) {
+    return false;
+  }
+
+  if (earliestCatalogTime && (0, _moment.default)(record.catalogDate).isBefore(earliestCatalogTime)) {
+    return false;
+  }
+
+  return true;
+
+  function checkLeader() {
+    if (!leader) {
+      return false;
+    }
+
+    if (leader.content[17] !== '4') {
+      return false;
+    }
+
+    if (['c', 'd', 'j'].includes(leader.content[6])) {
+      return false;
+    }
+
+    if (isMap()) {
+      return false;
+    }
+
+    return true;
+
+    function isMap() {
+      return leader.content[6] === 'a' && record.varFields.some(f => {
+        if (f.marcTag === '655') {
+          const a = f.subfields.find(sf => sf.tag === 'a');
+
+          if (a && a.content === 'kartastot') {
+            return true;
+          }
+        }
+
+        return false;
+      });
+    }
+  }
+
+  function isFromOverDrive() {
+    const f037 = record.varFields.filter(f => f.marcTag === '037');
+    const f710 = record.varFields.filter(f => f.marcTag === '710');
+    return f037.some(match037) || f710.some(match710);
+
+    function match037(f) {
+      const b = f.subfields.find(sf => sf.tag === 'b' && /^OverDrive/.test(sf.content));
+      const n = f.subfields.find(sf => sf.tag === 'n' && sf.content === 'http://www.overdrive.com');
+      return b && n;
+    }
+
+    function match710(f) {
+      return f.subfields.find(sf => sf.tag === 'a' && /^overdrive/i.test(sf.content));
+    }
+  }
+}
+//# sourceMappingURL=filter.js.map
